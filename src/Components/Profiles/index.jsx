cdef998529f8df91b6ee5d21cc70c39d2a17d3d6
swaaz <swaasthik.shetty07@gmail.com>
2020-10-13 01:33:15 +0530

    completed profiles

--- src/Components/Profiles/index.jsx
+++ src/Components/Profiles/index.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import Profile from './profile';
 import Styles from "./styles.module.scss";
 import {profileList} from '../profileList/profileList';
+import Logo from '../../assets/logo/gitifylogo.png';
 
 
 function Profiles() {
     return (
         <div className={Styles.profiles}>
+            <div className={Styles.header}>
+                <a href='/'>
+                <img src={Logo} alt='Gitify' />
+                </a>
+            </div>
             <div className={Styles.heading}>
                 <h1>Profiles</h1>
                 <div className={Styles.line}></div>
