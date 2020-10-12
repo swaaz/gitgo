5a0086b2358a2c81eed75b3550f24820d411ee8f
swaaz <swaasthik.shetty07@gmail.com>
2020-10-12 19:18:32 +0530

    added bg

--- src/Components/Landing/index.jsx
+++ src/Components/Landing/index.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import Navbar from "./Navbar";
 import Styles from "./styles.module.scss";
 import GitHub from '../../assets/logo/github.png';
+import Background from '../../assets/background/background.jpg'
 // import  from '../../assets/logo/github.png';
 
 function Landing() {
   return (
-    <div className={Styles.app}>
+    <section className={Styles.landing}>
+      {/* <div className={Styles.bg1} />
+      <div className={Styles.bg2} /> */}
+      <div className={Styles.background} />
+      <div className={Styles.app}>
+
+        <div className={Styles.header}>
+            <a href='swaaz.me'>
+              <img src={GitHub} alt='GitHub' />
+            </a>
+        </div>
+
         <div className={Styles.row}>
           <div>
             <Navbar />
@@ -15,12 +27,14 @@ function Landing() {
             <p>Gitify</p>
           </div>
         </div>
+
         <div className={Styles.footers}>
             <a href='swaaz.me'>
               <img src={GitHub} alt='GitHub' />
             </a>
         </div>
     </div>
+    </section>
   );
 }
 
