79dee860558dbd99b2f9c7d6cfd3f5593c5c17b1
Swasthik Shetty <42874695+swaaz@users.noreply.github.com>
2023-10-31 23:53:37 +0530

    code clean up

--- src/Components/profileList/profileList.js
+++ src/Components/profileList/profileList.js
@@ -130,12 +130,6 @@ export const profileList = [
        twitterUsername: '',
        linkedinURL: 'https://www.linkedin.com/in/manoj-naik-35a718202/',
     },
-    {
-        name: 'Rachitha Rai',
-        githubUsername: 'RachithaRai',
-        instagramUsername: 'rachitha_rai_',
-        twitterUsername: '',
-        linkedinURL: 'https://www.linkedin.com/in/rachitha-rai-70040317b/',
-    },
+   
 
 ]
